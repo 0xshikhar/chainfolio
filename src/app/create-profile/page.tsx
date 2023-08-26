292d2352e6bdba77fbe7309d73be6f8191012cc5
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-08-26 23:18:53 +0530

    updated create profile

--- src/app/create-profile/page.tsx
+++ src/app/create-profile/page.tsx
@@ -3,8 +3,10 @@
 import { useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import Skeleton from "@/components/Skeleton/Skeleton";
 import { useProvider, useAccount, useEnsName } from "wagmi";
+import { fetchEnsName } from "@wagmi/core";
 import styles from "@/styles/Profile.module.css";
 import randomColor from "@/lib/randomColor";
 import UserForm from "@/components/UserForm";
@@ -16,12 +18,27 @@ const [randomColor1, randomColor2, randomColor3, randomColor4] = [
     randomColor(),
 ];
 
-
 export default function CreateProfile() {
     const router = useRouter();
-    const { address, isConnected } = useAccount()    
+    const { address, isConnected } = useAccount()
     const provider = useProvider();
     console.log("provider", provider._network.chainId)
+    const [ensNameInfo, setEnsNameInfo] = useState() as any
+
+    useEffect(() => {
+        async function fetchData() {
+
+            const ensName = await fetchEnsName({
+                address: `${address}` as any,
+                chainId: 1,
+            })
+            console.log("ensName", ensName)
+            setEnsNameInfo(ensName)
+        }
+        if (isConnected) {
+            fetchData()
+        }
+    }, [address])
 
     // useEffect(() => {
     //     const fetchData = async () => {
@@ -37,7 +54,7 @@ export default function CreateProfile() {
     // }, [address]);
 
     return (
-        <div className="pt-4 h-full text-white bg-black">
+        <div className="pt-4 h-full text-black bg-gradient-to-r from-yellow-200 via-green-200 to-green-300">
             <div className="px-6 pt-4">
                 <div
                     className={styles.coverImage}
@@ -45,19 +62,22 @@ export default function CreateProfile() {
                         background: `linear-gradient(90deg, ${randomColor1}, ${randomColor2})`,
                     }}
                 />
-                <div
+                {/* <div
                     className={styles.profilePicture}
                     style={{
                         background: `linear-gradient(90deg, ${randomColor3}, ${randomColor4})`,
                     }}
-                />
+                /> */}
+                <div className={styles.profilePicture}>
+                    <Image src='/images/userProfile.webp' width={120} height={120} className="rounded-full bg-white" alt="user image" />
+                </div>
                 <h1 className={styles.profileName}>
                     <div>0xshikhar.eth</div>
                     {address}
                 </h1>
             </div>
             <div className="mx-10 pb-9">
-            <UserForm type="create" address={String(address)} />
+                <UserForm type="create" address={String(address)} />
 
             </div>
         </div>
